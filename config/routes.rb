1b6dbc2ce505f7bf2181fd8d0b2e85d40615f9d8
mayur.shivakumar <mayur.shivakumar@vonage.com>
2018-01-31 23:10:31 -0500

    added orders page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :products
   resources :order_items
   resources :category
+  resources :orders
   resource :carts, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
