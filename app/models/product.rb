0b8a13431e56ec8b570ab2de1d759388b8c502df
mayur.shivakumar <mayur.shivakumar@vonage.com>
2018-01-31 23:47:56 -0500

    added validation for products

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,4 @@
 class Product < ApplicationRecord
   belongs_to :category
+  validates_format_of :sku, :with=> /\A[A-Za-z0-9]+\Z/, :on => create, :message => "restricted to alphanumeric (a-z, 0-9) characters."
 end
